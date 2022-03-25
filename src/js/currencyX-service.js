dcfb9f141ff281cde913d145cde817d2dfc13920
Rosie Gardner <rosiegardner78@gmail.com>
2022-03-24 17:18:01 -0700

    add currencyX-service.js file

---
+++ src/js/currencyX-service.js
@@ -0,0 +1,39 @@
+export default class CurrencyXChange {
+  static async currencyRate() {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      if (!response.ok){
+        throw Error (response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//   export default class CurrencyXchange {
+//     static async xchangeCurrency(country, ) {
+//       console.log(country)
+//       try {
+//         const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+//       if (!response.ok) {
+//         throw Error (response.statusText);
+//       }
+//       return response.json();
+//     } catch(error) {
+//       return error.message;
+//     }
+//   }
+// }
