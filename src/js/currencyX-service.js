8038f9e36e59614deb932409b34d3cc36ecad504
Rosie Gardner <rosiegardner78@gmail.com>
2022-03-27 09:51:31 -0700

    update currencyX-service.js file

--- src/js/currencyX-service.js
+++ src/js/currencyX-service.js
@@ -11,29 +11,3 @@ export default class CurrencyXChange {
     }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-//   export default class CurrencyXchange {
-//     static async xchangeCurrency(country, ) {
-//       console.log(country)
-//       try {
-//         const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
-//       if (!response.ok) {
-//         throw Error (response.statusText);
-//       }
-//       return response.json();
-//     } catch(error) {
-//       return error.message;
-//     }
-//   }
-// }
