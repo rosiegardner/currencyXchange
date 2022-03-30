564f474933c2d799ce8b3a28e88483058153f120
Rosie Gardner <rosiegardner78@gmail.com>
2022-03-30 13:44:19 -0700

    compelete working UI and Business Logic enviorment

--- src/js/index.js
+++ src/js/index.js
@@ -2,42 +2,32 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/styles.css';
-import CurrencyXChange from './currencyX-service.js'
+import CurrencyXChange from './currencyX-service.js';
 
 function clearFields() {
   $('.showErrors').text(""); 
   $('.showCurrencyOutput').text("");
 }
 
-function getElements(response) {
-  let dollarInput = $('#dollarAmount').val();
-  let rateConversion = response.conversion_rates;
-  if (response) {
-    $('.showCurrencyOutput').append(`Your Currency XChange rate is ${dollarInput * rateConversion.CAD}`);
-   } else {
+function getElements(response, dollarInput, currency) {
+  let rateConversion = (response.conversion_rates[`${currency}`]);
+  if (typeof rateConversion === "number") {
+    $('.showCurrencyOutput').append(`Your Currency XChange rate is ${rateConversion}  $${dollarInput} USD is worth ${dollarInput*rateConversion} ${currency}`);
+  } else if (rateConversion === undefined){
+    $('.showErrors').text(`No data for ${currency}`);
+  } else {
     $('.showErrors').text(`There was an error: ${response.message}`);   
   }   
 }
 
-
-    // $('.showCurrencyOutput').append(`Your Currency XChange rate is ${rateObject.EUR}`); 
-    // $('.showCurrencyOutput').append(`Your Currency XChange rate is ${rateObject.DKK}`);
-    // $('.showCurrencyOutput').append(`Your Currency XChange rate is ${rateObject.NOK}`);
-    // $('.showCurrencyOutput').append(`Your Currency XChange rate is ${rateObject.SEK}`);
-    // $('.showCurrencyOutput').append(`Your Currency XChange rate is ${rateObject.CHF}`);
-    // $('.showCurrencyOutput').append(`Your Currency XChange rate is ${rateObject.KRW}`);
-    // $('.showCurrencyOutput').append(`Your Currency XChange rate is ${rateObject.KPW}`);
-    // $('.showCurrencyOutput').append(`Your Currency XChange rate is ${rateObject.BRL}`);
-    // $('.showCurrencyOutput').append(`Your Currency XChange rate is ${rateObject.COP}`);
-    // $('.showCurrencyOutput').append(`Your Currency XChange rate is ${rateObject.CUP}`);
-    // $('.showCurrencyOutput').append(`Your Currency XChange rate is ${rateObject.MXN}`);
-
 $(document).ready(function() {
   $('#showXChange').click(function() {
+    let dollarInput = $('#dollarAmount').val();
+    let currency = $('#xchangeCurrency').val();
     clearFields();
     CurrencyXChange.currencyRate()
-    .then(function(response) {
-      getElements(response);
-    });
+      .then(function(response) {
+        getElements(response, dollarInput, currency);
+      });
   });
 });
