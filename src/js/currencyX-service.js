55ea25569d61970c7e7dca9dbad3c5fe212b7e33
Rosie Gardner <rosiegardner78@gmail.com>
2022-03-30 13:43:33 -0700

    ready to push completed currencyX-service.js file

--- src/js/currencyX-service.js
+++ src/js/currencyX-service.js
@@ -1,7 +1,7 @@
 export default class CurrencyXChange {
   static async currencyRate() {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
       if (!response.ok) {
         throw Error (response.statusText);
       }
